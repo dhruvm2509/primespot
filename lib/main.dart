eea2138b661063ed0eaf1bf847daec57bdcd7a72
hrithik <hrithikvarshney98@gmail.com>
2021-10-20 12:51:51 +0530

    first page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'package:primespot/SplashScreen.dart';
+import 'package:primespot/Screens/SplashScreen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
