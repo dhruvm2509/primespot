b6e833492f027ffa23a6fa099a9746b337c95912
Dhruv <dhruvm2509@gmail.com>
2021-11-15 01:16:43 +0530

    Editing Product done

--- lib/Cards/AddProduct.dart
+++ lib/Cards/AddProduct.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:primespot/EditProduct/editProduct.dart';
 import 'package:primespot/Models/Products.dart';
 
 class ProductCard extends StatefulWidget {
@@ -10,68 +13,96 @@ class ProductCard extends StatefulWidget {
 }
 
 class _ProductCardState extends State<ProductCard> {
+  final FocusNode myFocusNode = FocusNode();
+
+  var id;
+
+  getProductId() {
+    FirebaseFirestore.instance
+        .collection(
+            'Seller/${FirebaseAuth.instance.currentUser!.phoneNumber}/Products')
+        .where('ProductName', isEqualTo: widget.products.productName)
+        .get()
+        .then((value) => {
+              value.docs.forEach((element) {
+                print(element.id);
+                id = element.id;
+              }),
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => EditProduct(id)))
+            });
+
+    print(id);
+  }
+
   String image =
       'https://www.mytrendyphone.eu/images/Haylou-LS02-Waterproof-Smartwatch-with-Heart-Rate-Black-6971664930443-11092020-01-p.jpg';
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 120.0,
-      child: Card(
-        color: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20.0),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            CircleAvatar(
-              backgroundColor: Colors.white,
-              radius: 55.0,
-              child: ClipOval(
-                child: Image(
-                  image: NetworkImage(widget.products.imageUrl == ""
-                      ? image
-                      : widget.products.imageUrl),
-                  fit: BoxFit.fitWidth,
+      child: InkWell(
+        onTap: () {
+          getProductId();
+        },
+        child: Card(
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: 55.0,
+                child: ClipOval(
+                  child: Image(
+                    image: NetworkImage(widget.products.imageUrl == ""
+                        ? image
+                        : widget.products.imageUrl),
+                    fit: BoxFit.fitWidth,
+                  ),
                 ),
               ),
-            ),
-            Container(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  // SizedBox(
-                  //   height: 5.0,
-                  // ),
-                  Text(
-                    ' ${widget.products.productName}',
-                    style: TextStyle(
-                        fontSize: 25.0,
-                        letterSpacing: 2.0,
-                        fontWeight: FontWeight.w900),
-                  ),
-                  SizedBox(
-                    height: 5.0,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 10.0),
-                    child: Text(
-                      ' Price : Rs.${widget.products.price}/-',
+              Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // SizedBox(
+                    //   height: 5.0,
+                    // ),
+                    Text(
+                      ' ${widget.products.productName}',
                       style: TextStyle(
-                          fontSize: 17.0,
-                          letterSpacing: 1.0,
-                          fontWeight: FontWeight.w800),
+                          fontSize: 25.0,
+                          letterSpacing: 2.0,
+                          fontWeight: FontWeight.w900),
                     ),
-                  ),
-                ],
+                    SizedBox(
+                      height: 5.0,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10.0),
+                      child: Text(
+                        ' Price : Rs.${widget.products.price}/-',
+                        style: TextStyle(
+                            fontSize: 17.0,
+                            letterSpacing: 1.0,
+                            fontWeight: FontWeight.w800),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
+          shadowColor: Colors.grey.shade100,
+          elevation: 20.0,
         ),
-        shadowColor: Colors.grey.shade100,
-        elevation: 20.0,
       ),
     );
+
     // return Padding(
     //   padding: const EdgeInsets.all(8.0),
     //   child: InkWell(
